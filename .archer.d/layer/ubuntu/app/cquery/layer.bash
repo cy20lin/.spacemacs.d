2dabf2b0fcc9f6954725b3d0664f591d9de403ec
ChienYu Lin <cy20lin@gmail.com>
2018-05-29 18:47:01 -0400

    Minor tweaks on archer layer /ubuntu/app/cquery
    * Fix building failure
    * Remove dependency on layer lang/python

--- .archer.d/layer/ubuntu/app/cquery/layer.bash
+++ .archer.d/layer/ubuntu/app/cquery/layer.bash
@@ -4,7 +4,6 @@ layer_help() {
 
 layer_metadata() {
     LAYER_DEPENDENCIES=(
-        lang/python
         app/gcc
         app/clang
         app/llvm
@@ -21,13 +20,13 @@ layer_install() {
     git clone https://github.com/cquery-project/cquery /tmp/archer/source/cquery
     mkdir -p /tmp/archer/build
     cp /tmp/archer/source/cquery /tmp/archer/build -R
-    pushd /tmp/archer/build/cquery
+    pushd /tmp/archer/build/cquery 1>/dev/null
     # git checkout v20180302
     git checkout 94a1a5c9d1911e919da3f5ab085fa3b938854505 # Fix bad memory usage by partially reverting
     git submodule update --init
-    mkdir build
-    cmake -Bbuild -H . -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+    mkdir build 2>/dev/null
+    cmake -Bbuild -H. -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
     cmake --build build
     sudo cmake --build build --target install
-    popd
+    popd 1>/dev/null
 }
