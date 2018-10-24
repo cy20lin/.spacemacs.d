7b3086c8cec2ba21af0ed0884f2f45421c338ecc
ChienYu Lin <cy20lin@gmail.com>
2018-10-24 13:49:11 -0400

    WIP Download emacs archive from github instead of cloning from git

--- .archer.d/layer/ubuntu/app/emacs/layer.bash
+++ .archer.d/layer/ubuntu/app/emacs/layer.bash
@@ -24,16 +24,23 @@ layer_install() {
        mkdir -p "${ARCHER_TMP}/source"
        git clone https://github.com/emacs-mirror/emacs "${ARCHER_TMP}/source/emacs"
     fi
+    test ! -d "${ARCHER_TMP}/archive/emacs" && mkdir -p "${ARCHER_TMP}/archive/emacs"
+    pushd "${ARCHER_TMP}/archive/emacs"
+    wget -O emacs.tar.gz https://github.com/emacs-mirror/emacs/tarball/00027ff9d0f646662458bdb47cc7e2214f439698
+    popd
+    test ! -d "${ARCHER_TMP}/source/emacs" && mkdir -p "${ARCHER_TMP}/source/emacs"
+    tar -xzf "${ARCHER_TMP}/archive/emacs/emacs.tar.gz" -C "${ARCHER_TMP}/source/emacs"
     test -d "${ARCHER_TMP}/build/emacs" && rm "${ARCHER_TMP}/build/emacs" -rf
     mkdir -p "${ARCHER_TMP}/build/emacs"
-    cp -a "${ARCHER_TMP}/source/emacs/." "${ARCHER_TMP}/build/emacs"
+    cp -a "${ARCHER_TMP}/source/emacs/emacs-mirror-emacs-00027ff/." "${ARCHER_TMP}/build/emacs"
     pushd "${ARCHER_TMP}/build/emacs" 1>/dev/null
-    git checkout 00027ff9d0f646662458bdb47cc7e2214f439698
 
     # read INSTALL.REPO
     ./autogen.sh
 
+    # TODO:
     # configure recommended I add --with-mailutils
+    # --with-module --with-xwidgets --with-modules --with-sound
     ./configure --with-mailutils
 
     make
