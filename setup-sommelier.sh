4ecd99c176d4ec1bc9dfcef507bfff73110428e5
ChienYu Lin <cy20lin@gmail.com>
2018-11-07 04:08:28 +0800

    Disable wayland backend for emacs using chromebook sommelier

--- setup-sommelier.sh
+++ setup-sommelier.sh
@@ -7,9 +7,13 @@
 # Reference:
 # https://github.com/dnschneid/crouton/wiki/Sommelier-%28A-more-native-alternative-to-xiwi%29
 
+# FIXME:
+# Cannot start emacs with wayland backend.
+# Disable these variables for now.
 
-export GDK_BACKEND=wayland
-export CLUTTER_BACKEND=wayland
-export XDG_RUNTIME_DIR='/var/run/chrome'
+# export GDK_BACKEND=wayland
+# export CLUTTER_BACKEND=wayland
+# export XDG_RUNTIME_DIR='/var/run/chrome'
+
 export WAYLAND_DISPLAY=wayland-0
 export DISPLAY=:0
